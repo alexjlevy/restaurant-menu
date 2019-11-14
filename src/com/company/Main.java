aace43b4ad569c9a4438d293443974e0fab56d6d
Alex Levy <alex.j.levy@gmail.com>
2019-11-14 12:14:27 -0600

    Studio created

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import restaurant.Menu;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Menu
+    }
+}
---
+++ src/restaurant/MenuItem.java
@@ -0,0 +1,44 @@
+package restaurant;
+
+public class MenuItem {
+
+    protected String itemName;
+    protected Double price;
+    protected String description;
+    protected String category;
+
+    public MenuItem (String itemName, Double price, String description, String category) {
+        this.itemName = itemName;
+        this.price = price;
+        this.description = description;
+        this.category = category;
+    }
+
+    public String getItemName (){
+        return itemName;
+    }
+    public void setItemName (String aItemName){
+        itemName = aItemName;
+    }
+
+    public Double getPrice (){
+        return price;
+    }
+    public void setPrice (Double aPrice){
+        price = aPrice
+    }
+
+    public String getDescription (){
+        return description;
+    }
+    public void setDescription (String aDescription){
+        description = aDescription;
+    }
+
+    public String getCategory (){
+        return category;
+    }
+    public void setCategory(String aCategory) {
+        this.category = aCategory;
+    }
+}
